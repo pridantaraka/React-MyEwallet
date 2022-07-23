5f99927ef986b2928aea9c92bc35ae7549e0f71e
pridantaraka <pridantaraka@gmail.com>
2022-07-23 15:34:16 +0700

    update auth and home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ import AlexHansinburg from '../assets/image/users/Alex Hansinburg.png'
 function Home() {
   return (
     <>
-        <body>
-            <header>
+        <body className='body-style'>
+            <header className='header-style'>
                 <nav>
                     <div className="nav-head">
                         <div className="mrg mrgtop">
@@ -28,7 +28,7 @@ function Home() {
                 <div className="head-flex">
                     <div className="head-centered">
                         <div>
-                            <h1 className="head-h1">Awesome App For <br/> Saving Time.</h1>
+                            <h1 className="head-h1 h1-home-style">Awesome App For <br/> Saving Time.</h1>
                         </div>
                         <p className="head-p p1">We bring you a mobile app for banking problems that <br/>
                             oftenly wasting much of your times.</p>
@@ -41,7 +41,7 @@ function Home() {
             <section>
                 <div className="mrg">
                     <div>
-                        <h1 className="sec1-h1"><span className="last-h1">Why</span> Choose MyEwallet?</h1>
+                        <h1 className="sec1-h1 h1-home-style"><span className="last-h1">Why</span> Choose MyEwallet?</h1>
                     </div>
                     <div>
                         <p className="sec1-p">We have some great features from the application and it`s totally free<br/>
@@ -69,7 +69,7 @@ function Home() {
                                 <img src={download} alt="logo"/>
                             </span>
                             <h3 className="sec1-box-h3">Easy Download</h3>
-                            <p className="sec1-box-p">Zwallet is 100% totally free to use it`s now available on 
+                            <p className="sec1-box-p">MyEwallet is 100% totally free to use it`s now available on 
                                 Google Play Store and App Store.</p>
                         </div>
                     </div>
@@ -85,10 +85,10 @@ function Home() {
             <section>
                 <div className="mrg newsec-flex">
                     <div className="border-m">
-                        <h1>Rp. 390.736.500</h1>
+                        <h1 className='h1-home-style'>Rp. 390.736.500</h1>
                     </div>
                     <div>
-                        <h1 className="newsec-h1"><span className="last-h1">Money</span> has Been Transfered.</h1>
+                        <h1 className="newsec-h1 h1-home-style"><span className="last-h1">Money</span> has Been Transfered.</h1>
                         <p className="p1 newsec-p">That amount of money has been transfered from all users. We still<br/>
                             counting and going strong!</p>
                     </div>
@@ -96,12 +96,12 @@ function Home() {
             </section>
             <section className="sec-bg">
                 <div className="sec3-flex">
-                    <div className="sec3-img">
+                    <div className="">
                         <img src={phone7} alt="phoneimg"/>
                     </div>
                     <div className="sec3-h1">
-                        <h1>All The <span className="last-h1">Great</span>
-                            Zwallet Features.</h1>
+                        <h1 className='h1-home-style'>All The <span className="last-h1">Great</span>
+                            MyEwallet Features.</h1>
                             <div className="sec3-box">
                                 <h3 className="sec3-h3"><span className="last-h1">1. </span> Small Fee</h3>
                                 <p className="p1">We only charge 5% of every success transaction done in Zwallet app.</p>
@@ -120,7 +120,7 @@ function Home() {
             <section className="mrg">
                 <div>
                     <div>
-                        <h1 className="sec1-h1">What Users are <span className="last-h1">Saying.</span></h1>
+                        <h1 className="sec1-h1 h1-home-style">What Users are <span className="last-h1">Saying.</span></h1>
                         <p className="p1 sec1-p">We have some great features from the application and it`s totally free<br/>
                         to use by all users around the world.</p>
                     </div>
@@ -143,17 +143,17 @@ function Home() {
             <footer className="footer-bg">
                 <div className="mrg">
                     <div>
-                        <h1 className="p-color footer-h1">Zwallet</h1>
+                        <h1 className="p-color footer-h1 h1-home-style">MyEwallet</h1>
                         <p className="p1 p-color">Simplify financial needs and saving <br/>
                             much time in banking needs with <br/>
                             one single app.</p>
                     </div>
                     <div></div>
                     <div className="footer-flex">
-                        <p className="footer-p">2020 Ewallet. All right reserved.</p>
+                        <p className="footer-p p-color">2020 Ewallet. All right reserved.</p>
                         <div className="ft-flex">
-                            <p className="footer-p">+62 5637 8882 9901</p>
-                            <p className="footer-p">contact@zwallet.com</p>
+                            <p className="footer-p p-color">+62 5637 8882 9901</p>
+                            <p className="footer-p p-color">contact@zwallet.com</p>
                         </div>
                     </div>
                 </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,74 +5,69 @@ import authphon from '../assets/image/authphon.png'
 
 function Login() {
     return(
-        <Container>
-            <Row>
-            <body className="auth-alltext">
-                <div className="row min-vh-100 mw-100">
-                   <Col md={7} className="auth-r-bg">
-                        <div className="d-flex flex-column auth-pad-cov auth-cov d-md-inline d-none">
-                            <div className="auth-ewallet">
-                                <span>Ewallet</span>
-                            </div>
-                            <div className="d-flex flex-column align-items-center">
-                                <div>
-                                    <img src={authphon} alt="phoneimg"/>
-                                </div>
-                                <div>
-                                    <h1 className=" auth-hcover">App that Covering Banking Needs.</h1>
-                                    <p className="auth-pcover">Zwallet is an application that focussing in banking needs for all users
-                                        in the world. Always updated and always following world trends.
-                                        5000+ users registered in Zwallet everyday with worldwide
-                                        users coverage.</p>
-                                </div>
-                            </div>
+        <>
+            <Container className='mw-100 min-vh-100'>
+                <Row className=''>
+                    <Col md={7} className="auth-r-bg">
+                    <section class="d-flex flex-column auth-pad-cov auth-cov d-md-block d-none">
+                        <div class="auth-ewallet">
+                            <span>MyEwallet</span>
                         </div>
-                    </Col>
-                    <Col md={5} sm={12}>
-                    <div className="auth-pad d-flex flex-column gap-5">
+                        <div class="d-flex flex-column align-items-center">
                         <div>
-                            <h1 className="auth-h1">Start Accessing Banking Needs
+                            <img src={authphon} alt="phoneimg"/>
+                        </div>
+                        <div>
+                            <h1 class="auth-hcover">App that Covering Banking Needs.</h1>
+                            <p class="auth-pcover">Zwallet is an application that focussing in banking needs for all users
+                                in the world. Always updated and always following world trends.
+                                5000+ users registered in Zwallet everyday with worldwide
+                                users coverage.</p>
+                        </div>
+                        </div>
+                    </section>
+                    </Col>
+                    <Col md={5} className="">
+                    <section class="auth-pad d-flex flex-column gap-5"> 
+                        <div>
+                            <h1 class="auth-h1">Start Accessing Banking Needs
                                 With All Devices and All Platforms
                                 With 30.000+ Users</h1>
-                            <div className="pt-5">
-                            <p className="auth-ptext">Transfering money is eassier than ever, 
-                            you can access Zwallet wherever you are. Desktop, laptop, 
-                            mobile phone? we cover all of that for you!</p>    
+                            <div class="pt-5">
+                            <p class="auth-ptext">Transfering money is eassier than ever, you can access
+                                Zwallet wherever you are. Desktop, laptop, mobile
+                                phone? we cover all of that for you!</p>    
                             </div>
                         </div>
-                        <div className="input-group flex-nowrap">
-                            <span className="input-group-text auth-icon-wr">
+                        <div class="input-group flex-nowrap">
+                            <span class="input-group-text auth-icon-wr">
                                 <i data-feather="mail"></i>
                             </span>
-                            <input type="email" className="form-control auth-input" placeholder="Enter your e-mail"/>
-                        </div>
-                        <div className="input-group flex-nowrap">
-                            <span className="input-group-text auth-icon-wr">
+                            <input type="email" class="form-control auth-input" placeholder="Enter your e-mail"/>
+                            </div>
+                            <div class="input-group flex-nowrap">
+                            <span class="input-group-text auth-icon-wr">
                                 <i data-feather="lock"></i>
                             </span>
-                            <input type="password" className="form-control auth-input" placeholder="Enter your password"/>
-                            <span className="input-group-text auth-icon-wr">
-                            <i data-feather="eye-off"></i>
+                            <input type="password" class="form-control auth-input" placeholder="Enter your password"/>
+                            <span class="input-group-text auth-icon-wr">
+                                <i data-feather="eye-off"></i>
                             </span>
-                        </div>
-                        <div className="text-end">
+                            </div>
+                            <div class="text-end">
                             <a href="resetpwd.html">Forgot Password?</a>
-                        </div>
-                        <div className="d-grid">
-                            <Button className="btn btn-primary auth-btn"><a className="nav-link" href="homepage.html">Login</a></Button>
-                        </div>
-                        <div className="text-center">
+                            </div>
+                            <div class="d-grid">
+                            <Button class="btn btn-primary auth-btn"><a class="nav-link" href="homepage.html">Login</a></Button>
+                            </div>
+                            <div class="text-center">
                             <span>Don`t have an account? Let`s <a href="signUp.html">Sign Up</a></span>
-                        </div>
-                    </div>
+                            </div>
+                    </section>
                     </Col>
-                </div>
-                <script>feather.replace()</script>
-                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-            </body>
-            </Row>
-        </Container>
+                </Row>
+            </Container>
+        </>
     )
 }
 
